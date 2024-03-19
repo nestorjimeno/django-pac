96ccb0a8fac0e3390cb3afd5f2987eca214f7ae7
nestor <jimeno_training@proton.me>
2024-03-19 21:25:43 +0100

    Corrijo errores de archivos

--- gestion_eventos/urls.py
+++ gestion_eventos/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-=======
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
->>>>>>> 7d9b0c63ea1d11041b4a145260cce2de97ca5e23
 ]
